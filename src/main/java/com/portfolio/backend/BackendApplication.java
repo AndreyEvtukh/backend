438c595fe2d224a14f31cc979698aead331fb107
AEvtukh <andrey.evtukh@gmail.com>
2026-08-12 13:01:13 +0300

    # CI13

--- src/main/java/com/portfolio/backend/BackendApplication.java
+++ src/main/java/com/portfolio/backend/BackendApplication.java
@@ -9,6 +9,7 @@ public class BackendApplication {
     public static void main(String[] args) {
         System.out.println("DB_USER=" + System.getenv("DB_USER"));
         System.out.println("DB_PASSWORD=" + System.getenv("DB_PASSWORD"));
+        System.out.println("RESEND_API_KEY=" + System.getenv("RESEND_API_KEY"));
         SpringApplication.run(com.portfolio.backend.BackendApplication.class, args);
     }
 }
